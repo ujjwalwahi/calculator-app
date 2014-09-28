917e0f4347edad0de0c066766780e6862806cc49
Ujjwal Wahi <w.ujjwal@gmail.com>
2014-09-29 01:03:06 +0530

    Updated UI

--- gen/com/example/calculator/R.java
+++ gen/com/example/calculator/R.java
@@ -1216,7 +1216,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_calculator=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int equal_button=0x7f030019;
+        public static final int numeric_pad=0x7f03001a;
+        public static final int operator_pad=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int calculator=0x7f0c0000;
